e22ba638e3dba088323e0b788c7ac45f185df9a5
landa.jiang <landa.jiang@horizon.ai>
2019-06-13 17:35:49 +0800

    delete xmlDecode function

--- src/get_lst_xml.py
+++ src/get_lst_xml.py
@@ -16,32 +16,6 @@ args.add_argument("-c", "--classFile",
                  help="path to txt file given classNames.")
 args = args.parse_args()
 
-#首先定义一个读取xml文件的函数：
-def xmlDecode(path, classNames):
-    annotation = minidom.parse(path)
-
-    size = annotation.getElementsByTagName('size')[0]
-    width = size.getElementsByTagName('width')[0].firstChild.data
-    height = size.getElementsByTagName('height')[0].firstChild.data
-
-    obj = annotation.getElementsByTagName('object')[0]
-    cla = obj.getElementsByTagName('name')[0].firstChild.data  #类别
-    bndbox = obj.getElementsByTagName('bndbox')[0]              #坐标
-    x1 = bndbox.getElementsByTagName('xmin')[0].firstChild.data
-    x2 = bndbox.getElementsByTagName('xmax')[0].firstChild.data
-    y1 = bndbox.getElementsByTagName('ymin')[0].firstChild.data
-    y2 = bndbox.getElementsByTagName('ymax')[0].firstChild.data
-    
-
-    width = int(width)
-    height = int(height)
-    x1 = int(x1)
-    x2 = int(x2)
-    y1 = int(y1)
-    y2 = int(y2)
-    result = [classNames.index(cla), (width,height), (x1,y1), (x2,y2)]
-    return result
-
 def write_line(img_path, im_shape, boxes, ids, idx):
     h, w = im_shape
     # for header, we use minimal length 2, plus width and height
@@ -101,8 +75,6 @@ if __name__ == "__main__":
     lst_file_name = args.lst
     #获取xml文件中出现的所有类别名称
     classNames = getClassNames(args.classFile)
-    #假设图片名和对应的标签名称一致，这里直接替换xml为jpg
-    #format:0  4  5  640(width)  480(height)  1(class)  0.1  0.2  0.8  0.9(xmin, ymin, xmax, ymax)  2  0.5  0.3  0.6  0.8  data/xxx.jpg
     names = os.listdir(path)
     lst = []
     i=0
